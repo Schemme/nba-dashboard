7a3a5a15c43f2932dfddf774e41e26b3ce61c008
Max Musial <144265654+masterpillow@users.noreply.github.com>
2024-04-29 14:52:16 -0400

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,17 +1,40 @@
 import streamlit as st
 
-st.write("# NBA Dashboard")
+# Configuring the page
+st.set_page_config(page_title="NBA Dashboard", layout="wide", page_icon="🏀")
+
+# Custom CSS to style the markdown
+st.markdown(
+    '''
+    <style>
+    .big-font {
+        font-size:20px !important;
+        font-weight: bold;
+    }
+    .nba-color {
+        color: #1d428a;  # Dark blue color; change as needed
+        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    }
+    </style>
+    ''', unsafe_allow_html=True
+)
 
 st.sidebar.success("Select an app above.")
 
-st.markdown(
-    """
-    This project was made in order to take in the overwhelming amount of NBA data and create meaningful visualizations from it.
-   
-    Please refer to the sidebar to the left in order to select a visualization.
-    """
-)
+col1, col2 = st.columns([3, 2])
+with col1:
+    st.markdown("## NBA Dashboard", unsafe_allow_html=True)
+    st.markdown("""
+        <div class='nba-color big-font'>This project was made to synthesize the overwhelming amount of NBA data into meaningful visualizations.</div>
+        """, unsafe_allow_html=True)
+
+with col2:
+    st.image("path_to_nba_image.png", width=300)  # Adjust path as necessary
+
+st.markdown("""
+    <div class='big-font'>Please refer to the sidebar to the left in order to select a visualization.</div>
+    """, unsafe_allow_html=True)
 
 # Adding a footer to credit the creators
 st.markdown("---")
-st.markdown("Created by Max Elliott and Brady")
+st.markdown("Created by **Max Elliott** and **Brady**")
