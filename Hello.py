669a43256f12f61003b5e905613fcce3ad96f252
Max Musial <144265654+masterpillow@users.noreply.github.com>
2024-04-29 14:54:20 -0400

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -3,16 +3,6 @@ import streamlit as st
 # Configuring the page
 st.set_page_config(page_title="NBA Dashboard", layout="wide", page_icon="🏀")
 
-# Apply a custom theme via your .streamlit/config.toml or directly in the code
-# You can uncomment and adjust the following lines to set a theme programmatically
-# st.experimental_set_theme({
-#     'primaryColor': '#1d428a',
-#     'backgroundColor': '#0e1117',
-#     'secondaryBackgroundColor': '#262730',
-#     'textColor': '#fafafa',
-#     'font': 'sans serif'
-# })
-
 # Custom CSS to style the markdown
 st.markdown(
     '''
@@ -21,9 +11,9 @@ st.markdown(
         font-size:20px !important;
         font-weight: bold;
     }
-    .info-text {
-        font-size:16px;
-        color: #f63366;  # Bright color for emphasis
+    .nba-color {
+        color: #1d428a;  # Dark blue color; change as needed
+        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
     }
     </style>
     ''', unsafe_allow_html=True
@@ -31,27 +21,17 @@ st.markdown(
 
 st.sidebar.success("Select an app above.")
 
-st.title("NBA Dashboard")
+col1, col2 = st.columns([3, 2])
+with col1:
+    st.markdown("## NBA Dashboard", unsafe_allow_html=True)
+    st.markdown("""
+        <div class='nba-color big-font'>This project was made to synthesize the overwhelming amount of NBA data into meaningful visualizations.</div>
+        """, unsafe_allow_html=True)
 
 st.markdown("""
-    <div class='big-font info-text'>This project was made to synthesize the overwhelming amount of NBA data into meaningful visualizations.</div>
+    <div class='big-font'>Please refer to the sidebar to the left in order to select a visualization.</div>
     """, unsafe_allow_html=True)
 
-st.markdown("""
-    <div class='info-text'>Please refer to the sidebar to the left in order to select a visualization.</div>
-    """, unsafe_allow_html=True)
-
-# Adding a footer to credit the creators with enhanced styling
+# Adding a footer to credit the creators
 st.markdown("---")
-st.markdown("Created by **Max Elliott** and **Brady** with :heart:")
-
-# Example of adding interactive elements
-team_select = st.sidebar.selectbox("Choose a team to display stats:", ["Lakers", "Warriors", "Celtics", "Bulls"])
-if team_select:
-    st.write(f"You selected the {team_select}, here are the stats...")
-
-# Placeholder for dynamic data visualization
-# This could be a place to add charts or data tables based on the team selection
-st.write("Chart or data table will go here based on the team selected.")
-
-
+st.markdown("Created by **Max Elliott** and **Brady**")
