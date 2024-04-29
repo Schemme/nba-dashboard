ad90799bfce621e2e48689b1ddc68e0d851d41ff
Max Musial <144265654+masterpillow@users.noreply.github.com>
2024-04-29 14:50:20 -0400

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,9 +1,5 @@
 import streamlit as st
 
-# Provide an absolute path if relative paths fail
-nba_logo_path = "/absolute/path/to/nba-dashboard/nba_logo_2024.png"
-st.image(nba_logo_path, caption='NBA Logo 2024', width=300)
-
 st.write("# NBA Dashboard")
 
 st.sidebar.success("Select an app above.")
