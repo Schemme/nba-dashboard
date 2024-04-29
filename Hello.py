3d90100ca38c3a0c616ffff99e353f8d69026d40
Max Musial <144265654+masterpillow@users.noreply.github.com>
2024-04-29 14:40:03 -0400

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
+# Load and display an image
+nba_logo_path = "nba_logo_2024.png"  # If locally saved, or replace with a URL if hosted online
+st.image(nba_logo_path, caption='NBA Logo 2024')
+
 st.write("# NBA Dashboard")
 
 st.sidebar.success("Select an app above.")
